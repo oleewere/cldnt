efb984276ae2cc65acb98c8b67f0eecbe612839a
Oliver Szabo <oleewere@gmail.com>
2019-08-17 20:04:35 +0200

    Use public ip by default

--- cli/airport.go
+++ cli/airport.go
@@ -14,13 +14,15 @@ func ListAirportsCommand() cli.Command {
 		Name:  "airport",
 		Usage: "Get closest airports",
 		Action: func(c *cli.Context) error {
-			if c.Bool("ip") {
-				client.CalculateLocationFromIP()
-			} else if len(c.String("la")) > 0 && len(c.String("lo")) > 0 {
+			if len(c.String("la")) > 0 && len(c.String("lo")) > 0 {
 
 			} else {
-				fmt.Println()
-				os.Exit(1)
+				location, err := client.CalculateLocationFromIP()
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+				fmt.Println(location)
 			}
 			return nil
 		},
@@ -28,7 +30,6 @@ func ListAirportsCommand() cli.Command {
 			cli.StringFlag{Name: "airport-db-search-url, a", Usage: "Search URL for airport DB", Value: "https://mikerhodes.cloudant.com/airportdb/_design/view1/_search/geo", Destination: &airportSearchUrl},
 			cli.StringFlag{Name: "latitude, la", Usage: "Latitude parameter for calculating nearest airports"},
 			cli.StringFlag{Name: "longitude, lo", Usage: "Longitude parameter for calculating nearest airports"},
-			cli.BoolFlag{Name: "use-ip-address, ip", Usage: "Use IP address to calculate latitude and longitude"},
 			cli.IntFlag{Name: "rows, r", Usage: "Maximum number of rows to show."},
 		},
 	}
