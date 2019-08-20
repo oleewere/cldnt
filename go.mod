0d9634895e2dab13629e3bf9970db882c25bf2f1
Oliver Szabo <oleewere@gmail.com>
2019-08-20 17:11:41 +0200

    Add airports web endpoint

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.12
 require (
 	cloud.google.com/go v0.44.3 // indirect
 	github.com/coreos/bbolt v1.3.3 // indirect
-	github.com/coreos/etcd v3.3.13+incompatible // indirect
+	github.com/coreos/etcd v3.3.15+incompatible // indirect
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/coreos/go-systemd v0.0.0-20190719114852-fd7a80b32e1f // indirect
 	github.com/go-kit/kit v0.9.0 // indirect
@@ -45,8 +45,8 @@ require (
 	golang.org/x/mobile v0.0.0-20190814143026-e8b3e6111d02 // indirect
 	golang.org/x/net v0.0.0-20190813141303-74dc4d7220e7 // indirect
 	golang.org/x/sys v0.0.0-20190813064441-fde4db37ae7a // indirect
-	golang.org/x/tools v0.0.0-20190816200558-6889da9d5479 // indirect
-	google.golang.org/genproto v0.0.0-20190817000702-55e96fffbd48 // indirect
+	golang.org/x/tools v0.0.0-20190820033707-85edb9ef3283 // indirect
+	google.golang.org/genproto v0.0.0-20190819201941-24fa4b261c55 // indirect
 	google.golang.org/grpc v1.23.0 // indirect
 	honnef.co/go/tools v0.0.1-2019.2.2 // indirect
 )
--- web/router.go
+++ web/router.go
@@ -3,10 +3,14 @@ package web
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gobuffalo/packr"
 	"github.com/gorilla/mux"
+	"github.com/oleewere/cldnt/client"
+	"github.com/oleewere/cldnt/model"
 )
 
 // StartServer starts web server
@@ -14,11 +18,53 @@ func StartServer(port int) {
 	box := packr.NewBox("../static/")
 	router := mux.NewRouter()
 	router.HandleFunc("/ping", PingHandler)
+	router.HandleFunc("/airports", AirportsHandler)
 	router.PathPrefix("/").Handler(http.FileServer(box))
+	log.Println(fmt.Sprintf("Start server on port %v", port))
 	http.ListenAndServe(fmt.Sprintf(":%v", port), router)
 }
 
 // PingHandler send a simple ping back by the web server
 func PingHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+}
+
+// AitportsHandler get closest airports
+func AirportsHandler(w http.ResponseWriter, r *http.Request) {
+	lat := r.FormValue("lat")
+	lon := r.FormValue("lon")
+	rowStr := r.FormValue("rows")
+	var rows int = 10
+	var rowError error
+	if len(rowStr) > 0 {
+		rows, rowError = strconv.Atoi(rowStr)
+		handleError(rowError, w)
+	}
+	if rows > 200 {
+		rows = 200
+	}
+	var location model.Location
+	if len(lat) == 0 || len(lon) == 0 {
+		lotLatPair, err := client.CalculateLocationFromIP()
+		handleError(err, w)
+		location = *lotLatPair
+	} else {
+		latutide, err := strconv.ParseFloat(lat, 64)
+		handleError(err, w)
+		longitude, err := strconv.ParseFloat(lon, 64)
+		handleError(err, w)
+		location = model.Location{Latitude: latutide, Longitude: longitude}
+	}
+	log.Println(fmt.Sprintf("Inputs - rows: %d - latitude: %f - longitue: %f", rows, location.Latitude, location.Longitude))
+	airports, errAirp := client.ListAirportsByDistance(location, rows, "")
+	log.Println(fmt.Sprintf("Airports response: %v", airports))
+	handleError(errAirp, w)
+	json.NewEncoder(w).Encode(airports)
+}
+
+func handleError(err error, w http.ResponseWriter) {
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
 }
